28d183e29e1e4832f6ec67fd34b45262c4ff8a9d
James3 Li(李麒傑) <james3_li@asus.com>
2020-09-24 18:39:19 +0800

    refactor infra

--- metadata/provider.go
+++ metadata/provider.go
@@ -2,8 +2,6 @@ package metadata
 
 import (
 	"github.com/pegasus-cloud/metadata_client/metadata/common"
-	"github.com/pegasus-cloud/metadata_client/metadata/elasticsearch"
-	"github.com/spf13/viper"
 )
 
 // Metadata ...
@@ -17,54 +15,16 @@ type Metadata interface {
 	HealthCheck() (status string, err error)
 }
 
-const (
-	// ElasticsearchProviderType declares that metadata provider is elasticsearch
-	ElasticsearchProviderType = "elasticsearch"
-)
+var metadataProvider Metadata
 
-// MetadataProvider ...
-var MetadataProvider Metadata
+// type MD interface{}
 
 // Init ...
-func Init(providerName string) {
-	MetadataProvider = setConfig(providerName)
-}
-
-// New ...
-func New(providerName string) (MetadataProvider Metadata) {
-	return setConfig(providerName)
-}
-
-func setConfig(providerName string) (metadataProvider Metadata) {
-	var provider Metadata
-	switch providerName {
-	case ElasticsearchProviderType:
-		esProvider := &elasticsearch.Provider{
-			Schema:       elasticsearch.Schema,
-			Endpoint:     elasticsearch.Endpoint,
-			Index:        elasticsearch.Index,
-			DeletedIndex: elasticsearch.DeletedIndex,
-		}
-		if viper.GetString("metadata.elasticsearch.schema") == "" {
-			esProvider.Schema = viper.GetString("metadata.elasticsearch.schema")
-		}
-		if viper.GetString("metadata.elasticsearch.endpoint") == "" {
-			esProvider.Endpoint = viper.GetString("metadata.elasticsearch.endpoint")
-		}
-		if viper.GetString("metadata.elasticsearch.attribute_indices") == "" {
-			esProvider.Index = viper.GetString("metadata.elasticsearch.attribute_indices")
-		}
-		if viper.GetString("metadata.elasticsearch.msg_deleted_indices") == "" {
-			esProvider.DeletedIndex = viper.GetString("metadata.elasticsearch.msg_deleted_indices")
-		}
-		provider = esProvider
-	default:
-		panic(common.ProviderNameDoesNotSupport)
-	}
-	return provider
+func Init(provider Metadata) {
+	metadataProvider = provider
 }
 
 // Use ...
 func Use() (Metadata Metadata) {
-	return MetadataProvider
+	return metadataProvider
 }
